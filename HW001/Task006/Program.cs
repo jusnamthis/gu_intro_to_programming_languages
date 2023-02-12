269549ead83e14671a187436615dcac86e08cb73
Karvyad <karvyad@yandex.ru>
2023-02-12 22:25:54 +0700

    Task006 done

---
+++ HW001/Task006/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace Task006;
+class Program
+{
+    static void Main(string[] args)
+    {
+        IsNumEven();
+    }
+
+    static void IsNumEven()
+    {
+        Console.Write("Enter the number: ");
+        var x = Convert.ToInt32(Console.ReadLine());
+        IsNumEven(x);
+    }
+
+    static void IsNumEven(int x)
+    {
+        if (x % 2 == 0)
+        {
+            Console.WriteLine("The number is even.");
+        }
+        else
+        {
+            Console.WriteLine("The number is uneven.");
+        }
+    }
+}
