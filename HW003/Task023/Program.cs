81e4f8db1225b89f7f8cb9944b111b4414b97e2c
Vyacheslav Kart <KartashovVD@yandex.ru>
2023-02-21 00:18:29 +0700

    Task023 done.

---
+++ HW003/Task023/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Task023;
+class Program
+{
+    // Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+
+    static void Main(string[] args)
+    {
+        showSequenceOfCubes();
+    }
+
+    static void showSequenceOfCubes()
+    {
+        var seqLimit = new Random().Next(1, 20);
+
+        for (var i = 1; i < seqLimit; i++)
+        {
+            Console.Write($"{Math.Pow(i, 3)}, ");
+        }
+        Console.WriteLine($"{Math.Pow(seqLimit, 3)}");
+    }
+}
