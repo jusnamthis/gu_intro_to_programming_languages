9c8ca343054b836edeed5ed7bbdd01803594e110
Vyacheslav Kart <KartashovVD@yandex.ru>
2023-02-20 23:30:21 +0700

    Task015 done.

---
+++ HW002/Task015/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace Task015;
+class Program
+{
+    static void Main(string[] args)
+    {
+        checkIfTheDayIsWeekend();
+    }
+
+    static void checkIfTheDayIsWeekend()
+    {
+        var dayNum = new Random().Next(1, 8);
+        checkIfTheDayIsWeekend(dayNum);
+    }
+
+    static void checkIfTheDayIsWeekend(int dayNum)
+    {
+        Console.WriteLine($"Day to check is -> {dayNum}");
+        switch (dayNum)
+        {
+            case 1:
+            case 2:
+            case 3:
+            case 4:
+            case 5:
+                Console.WriteLine("This day is not weekend day!");
+                break;
+            case 6:
+            case 7:
+                Console.WriteLine("This day is weekend day!");
+                break;
+            default:
+                Console.WriteLine("Wrong day of the week...");
+                break;
+        }
+
+    }
+}
