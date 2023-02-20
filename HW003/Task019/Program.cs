98d3c6e9eb295bd76ddd0ffc8ad3e1225978e1f9
Vyacheslav Kart <KartashovVD@yandex.ru>
2023-02-20 23:46:59 +0700

    Task019 done.

---
+++ HW003/Task019/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace Task019;
+class Program
+{
+    // Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+
+    static void Main(string[] args)
+    {
+        checkForPolindrome(23432);
+    }
+
+    static void checkForPolindrome()
+    {
+        var numToCheck = new Random().Next(10000, 100000);
+        checkForPolindrome(numToCheck);
+    }
+
+    static void checkForPolindrome(int num)
+    {
+        var originNum = num;
+        var reversedNum = 0;
+        while (num != 0)
+        {
+            reversedNum = reversedNum * 10 + num % 10;
+            num /= 10;
+        }
+
+        if (reversedNum == originNum)
+        {
+            Console.WriteLine($"The number {originNum} is polindrome");
+        }
+        else
+        {
+            Console.WriteLine($"The number {originNum} is not polindrome");
+        }
+    }
+}
