aea87e5703f0c70efe111aed47cdc430a69a9c6d
Vyacheslav Kart <KartashovVD@yandex.ru>
2023-02-25 00:16:08 +0700

    Task027 done

---
+++ HW004/Task027/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace Task027;
+class Program
+{
+
+    // Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+    static void Main(string[] args)
+    {
+        GetNumberAndCalculateSumOfItsDigits();
+    }
+
+    static int GetNumber(string message)
+    {
+        Console.Write(message);
+        var num = Convert.ToInt32(Console.ReadLine());
+        return num;
+    }
+
+    static void GetNumberAndCalculateSumOfItsDigits()
+    {
+        var num = GetNumber("Enter the number to calculate sum of its digits: ");
+        var res = 0;
+
+        while (num > 0)
+        {
+            res += num % 10;
+            num /= 10;
+        }
+
+        Console.WriteLine($"{res} is a sum of your digits");
+    }
+}
