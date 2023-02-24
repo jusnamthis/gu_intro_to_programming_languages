708f1546a2d0a926107255a4bccea7f24282cf88
Vyacheslav Kart <KartashovVD@yandex.ru>
2023-02-25 00:30:54 +0700

    task029 done

---
+++ HW004/Task029/Program.cs
@@ -0,0 +1,25 @@
+﻿// Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+
+var rnd = new Random();
+
+void GetNumberAndPushItToArr(int arrIndex, int[] arr)
+{
+    var n = rnd.Next(1, 100);
+    arr[arrIndex] = n;
+}
+
+void PrintArr(int[] arr)
+{
+    foreach (var el in arr)
+    {
+        Console.Write($"{el} ");
+    }
+}
+
+int[] arr = new int[8];
+for (int i = 0; i < arr.Length; i++)
+{
+    GetNumberAndPushItToArr(i, arr);
+}
+
+PrintArr(arr);
